ba1af89897fd4f6ecc20534ef77f189334a8e659
steffen123 <steffen@sycamoretest.info>
2008-09-24 04:22:59 +0100

    p100 - updated contacts with mailing list and how to subscribe to new downloads

--- website/contact.php
+++ website/contact.php
@@ -9,14 +9,16 @@ require 'sidebar.php';
 
                 <h1>Contact</h1>
 
-                <p>The best means of contact are the <a href="http://sourceforge.net/projects/fpdb/">sourceforge page</a>: Use the bug, feature request or patch functions or just post in the forum.</p>
+                <p>The best means of contact are on the <a href="http://sourceforge.net/projects/fpdb/">sourceforge page</a>: Use the bug, feature request or patch functions or just post in the forum. The forum requires a sourceforge account, but bug, feature request and patches don't seem to.</p>
+<p>We also have a mailing list, to subscribe please <a href="https://lists.sourceforge.net/lists/listinfo/fpdb-main">click here</a>. This is used for development purposes and you can also ask for help using the mailing list.</p>
+<p>To be informed of fpdb updates please visit <a href="https://sourceforge.net/project/showfiles.php?group_id=226872">the sf download page</a> and click on the little envelope with a plus sign in the line of the package that you want be informed off. For Windows user that will be both, Linux users should only subscribe to fpdb itself. This requires a sourceforge account, sorry.</p>
 
                 <p>Alternatively feel free to contact me directly:</p>
 
 <p>mail: steffen(at)sycamoretest.info<br>
-jabber/xmpp/Google Talk: as above<br>
+jabber/xmpp/Google Talk: steffen-laptop-temp(at)sycamoretest.info (don't email that)<br>
 ICQ: 7806355<br>
-MSN: steffenjf(at)gmx.de (don't email that)</p>
+MSN: steffen(at)sycamoretest.info</p>
 
             </div>
 
