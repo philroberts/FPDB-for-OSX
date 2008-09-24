c4f6ac42a093da8a94d0b004d66fe3fa65dc463a
steffen123 <steffen@sycamoretest.info>
2008-09-24 03:51:47 +0100

    p97 - new banner from Bar Nuthin and updated requirements from me

--- website/docs-requirements.php
+++ website/docs-requirements.php
@@ -13,24 +13,21 @@ require 'sidebar.php';
 <p>
 I recommend using a free/libre operating system, meaning a GNU/Linux distribution or a BSD variant (e.g. Gentoo GNU/Linux or OpenBSD) for ethical and practical reasons. Would you buy a car where you're prohibited from opening the bonnet under threat of jail? If the answer is no you should by the same logic not use closed source software for real money Poker :)<br>
 <br>
-Unfortunately you will always need one piece of unfree software: The poker client itself. Although not a direct dependency of fpdb you obviously will have a hard time putting this to productive use without running some poker client. As far as I know, only unfree clients are available. If you know better please let me know ASAP!<br>
+Unfortunately you will always need one piece of unfree software: The poker client itself. Although not a direct dependency of fpdb you obviously will have a hard time putting this to productive use without running some poker client. As far as I know, only unfree clients are available. If you know better please let me know!<br>
 <br>
-If you can be bothered please do contact your poker site(s) and ask them to release free/libre clients, even if it is only for Windows. But lets be realistic, the chance of a positive answer is very low.<br>
+If you can be bothered please do contact your poker site(s) and ask them to release free/libre clients, even if it is only for Windows. But lets be realistic, the chance of a positive answer is very low. Also, even unfree Linux client would of course be a great step forward<br>
 <br>
-Before I start the list a note on the databases, as of git96 I have yet to try using this with PostgreSQL, but if I'm not mistaken it should actually work by now (the stuff in fpdb-python at least).<br>
+In Windows use of the environment installer is recommended, pls see our sf download page. For Gentoo Linux we have an ebuild and for Ubuntu Linux we have (partial) instructions. If you use a different Linux or a BSD and have trouble please IM, email or post in the forums. Fpdb has been reported to work on MacOSX, but installation of the requirements is relatively painful. Any instructions for people to use would be much appreciated.<br>
 <br>
-If you use a package management system (e.g. if you have GNU/Linux or *BSD) just check that you have mysql, mysql-python and pygtk or postgresql, pygresql and pygtk. Your package manager will take care of the rest for you.<br>
-	<br>
 Make new entries in this format:<br>
-X. Program Name<br>
-===============<br>
+<b>Program Name</b><br>
 a. Optional?<br>
 b. Required Version and Why<br>
 c. Project Webpage<br>
-d. License<br>
-<br>
-1. MySQL<br>
-========<br>
+d. License</p>
+<h2>Database backend - MySQL</h2>
+<p>These two are required if you want to use MySQL as backend, which is the recommended choice due to lack of testing and polish of PostgreSQL support.</p>
+<p><b>MySQL</b><br>
 a. Optional?<br>
 	Choose MySQL or PostgreSQL<br>
 b. Required Version and Why<br>
@@ -41,8 +38,20 @@ c. Project Webpage<br>
 d. License<br>
 	GPL2<br>
 <br>
-2. PostgreSQL<br>
-=============<br>
+<b>mysql-python</b><br>
+a. Optional?<br>
+	Required if you want to use MySQL backend<br>
+b. Required Version and Why<br>
+	I use 1.2.2 but I am not aware of any incompatibilities with older or newer versions, pls report success/failure.<br>
+c. Project Webpage<br>
+	http://sourceforge.net/projects/mysql-python/<br>
+d. License<br>
+	SF lists GNU General Public License (GPL), Python License (CNRI Python License), Zope Public License.<br>
+	Project states GPL without version in Pkg-info.</p>
+	
+<h2>Database backend - PostgreSQL</h2>
+<p>These two are required if you want to use PostgreSQL as backend</p>
+<p><b>PostgreSQL</b><br>
 a. Optional?<br>
 	Choose MySQL or PostgreSQL<br>
 b. Required Version and Why<br>
@@ -53,34 +62,18 @@ c. Project Webpage<br>
 d. License<br>
 	BSD License<br>
 <br>
-3. mysql-python<br>
-===============<br>
-a. Optional?<br>
-	Required if you want to use MySQL backend<br>
-b. Required Version and Why<br>
-	I use 1.2.2 but I am not aware of any incompatibilities with older or newer versions, pls report success/failure.<br>
-c. Project Webpage<br>
-	http://sourceforge.net/projects/mysql-python/<br>
-d. License<br>
-	SF lists GNU General Public License (GPL), Python License (CNRI Python License), Zope Public License.<br>
-	Project states GPL without version in Pkg-info.<br>
-<br>
-4. pygresql<br>
-===========<br>
+<b>psycopg</b><br>
 a. Optional?<br>
 	Required if you want to use PostgreSQL backend<br>
 b. Required Version and Why<br>
-	I use 3.6.2 but I am not aware of any incompatibilities with older or newer versions, pls report success/failure.<br>
+	I use 2.0.2 but I am not aware of any incompatibilities with older or newer versions, pls report success/failure.<br>
 c. Project Webpage<br>
-	http://www.pygresql.org/<br>
+	http://initd.org/projects/psycopg2<br>
 d. License<br>
-	http://www.pygresql.org/readme.html#copyright-notice (BSD License?)<br>
-	Summary: "Permission to use, copy, modify, and distribute this software and its <br>
-	documentation for any purpose, without fee, and without a written agreement <br>
-	is hereby granted[...]" plus Disclaimer.<br>
+	GPL2 according to Gentoo's ebuilds<br>
 <br>
-5. Python<br>
-=========<br>
+<h2>Required for everyone</h2>
+<p><b>Python</b><br>
 a. Optional?<br>
 	Required.<br>
 b. Required Version and Why<br>
@@ -90,8 +83,7 @@ c. Project Webpage<br>
 d. License<br>
 	Python License<br>
 <br>
-6. GTK+ and dependencies<br>
-=======<br>
+<p><b>GTK+ and dependencies</b><br>
 a. Optional?<br>
 	Required.<br>
 b. Required Version and Why<br>
@@ -103,8 +95,7 @@ c. Project Webpage<br>
 d. License<br>
 	LGPL2<br>
 <br>
-7. PyCairo<br>
-==========<br>
+<b>PyCairo</b><br>
 a. Optional?<br>
 	Required.<br>
 b. Required Version and Why<br>
@@ -114,8 +105,7 @@ c. Project Webpage<br>
 d. License<br>
 	LGPL2.1<br>
 <br>
-8. PyGObject<br>
-============<br>
+<b>PyGObject</b><br>
 a. Optional?<br>
 	Required.<br>
 b. Required Version and Why<br>
@@ -125,8 +115,7 @@ c. Project Webpage<br>
 d. License<br>
 	LGPL2.1<br>
 <br>
-9. PyGTK<br>
-========<br>
+<b>PyGTK</b><br>
 a. Optional?<br>
 	Required.<br>
 b. Required Version and Why<br>
@@ -134,8 +123,29 @@ b. Required Version and Why<br>
 c. Project Webpage<br>
 	main: http://www.pygtk.org<br>
 d. License<br>
-	LGPL2.1<br>
-<br>
+	LGPL2.1</p>
+<h2>Requirements for the graphing function</h2>
+<p>These are only required if you wish to use the graphing function, and fpdb will otherwise function without them</p>
+<p><b>Numpy</b><br>
+a. Optional?<br>
+	Optional.<br>
+b. Required Version and Why<br>
+	I use 1.0.4 but I am not aware of any incompatibilities with older or newer versions, pls report success/failure.<br>
+c. Project Webpage<br>
+	http://numeric.scipy.org/<br>
+d. License<br>
+	BSD according to Gentoo's ebuild<br>
+	<br>
+<b>matplotlib</b><br>
+a. Optional?<br>
+	Optional.<br>
+b. Required Version and Why<br>
+	I use 0.91.2 but I am not aware of any incompatibilities with older or newer versions, pls report success/failure.<br>
+c. Project Webpage<br>
+	http://matplotlib.sourceforge.net/<br>
+d. License<br>
+	BSD according to Gentoo's ebuild</p>
+
 License (of this file)<br>
 =======<br>
 Trademarks of third parties have been used under Fair Use or similar laws.<br>
