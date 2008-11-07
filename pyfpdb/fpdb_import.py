c7393951893011df98986bd89b82af4df08d33cc
steffen123 <steffen@sycamoretest.info>
2008-11-07 00:14:25 +0000

    p140 - added CLI option to fpdb.py to control redirection of stderr

--- pyfpdb/fpdb_import.py
+++ pyfpdb/fpdb_import.py
@@ -292,4 +292,4 @@ class Importer:
 	
 
 if __name__ == "__main__":
-	print "CLI for fpdb_import is currently on vacation please check in later"
+	print "CLI for fpdb_import is now available as CliFpdb.py"
