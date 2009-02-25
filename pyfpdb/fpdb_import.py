e5489056ff86fd113e385752dc519743ab83e99b
Matt Turnbull <mcturnbull@fastmail.fm>
2009-02-25 14:59:11 +0000

    less broken, please check sanity

--- pyfpdb/fpdb_import.py
+++ pyfpdb/fpdb_import.py
@@ -113,6 +113,20 @@ class Importer:
         #TODO: test it is a valid file -> put that in config!!
         self.filelist[filename] = [site] + [filter]
 
+    # Called from GuiBulkImport to add a file or directory.
+    def addBulkImportImportFileOrDir(self, inputPath,filter = "passthrough"):
+        """Add a file or directory for bulk import"""
+        # Bulk import never monitors
+        
+        # if directory, add all files in it. Otherwise add single file.
+        # TODO: only add sane files?
+        if os.path.isdir(inputPath):
+            for subdir in os.walk(inputPath):
+                for file in subdir[2]:
+                    self.addImportFile(os.path.join(inputPath, subdir, file), site="default", filter=filter)
+        else:
+            self.addImportFile(inputPath, site="default", filter=filter)
+
     #Add a directory of files to filelist
     #Only one import directory per site supported.
     #dirlist is a hash of lists:
