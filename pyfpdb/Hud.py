6d4c812b51a07ed55b711299c95f09bc8b5fbefe
eblade <blade.eric@gmail.com>
2009-03-26 12:11:07 -0400

    do that iteration in relocate_windows the proper way

--- pyfpdb/Hud.py
+++ pyfpdb/Hud.py
@@ -175,16 +175,9 @@ class Hud:
             adj = self.adj_seats(self.hand, self.config)
             loc = self.config.get_locations(self.table.site, self.max)
             # TODO: is stat_windows getting converted somewhere from a list to a dict, for no good reason?
-            for i, w in enumerate(self.stat_windows):
-#<<<<<<< HEAD:pyfpdb/Hud.py
-                if not type(w) == int: # how do we get pure ints in this list??
-                    (x, y) = loc[adj[i]]
-                    w.relocate(x, y)
-#=======
-#                (x, y) = loc[adj[i]]
-#                self.stat_windows[w].relocate(x, y)
-#                
-#>>>>>>> 7c0d2eb6c664cfd8122b975e58438cfd158ee398:pyfpdb/Hud.py
+            for i, w in enumerate(self.stat_windows.itervalues()):
+                (x, y) = loc[adj[i]]
+                w.relocate(x, y)
         return True
 
     def on_button_press(self, widget, event):
