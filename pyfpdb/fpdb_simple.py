0528aa74dbc131f0dd69098e51ee79f062f7c1df
Worros <carl.gherardi@gmail.com>
2009-03-29 00:07:00 +0900

    Properly escape player names

--- pyfpdb/fpdb_simple.py
+++ pyfpdb/fpdb_simple.py
@@ -1393,13 +1393,15 @@ def recogniseTourneyTypeId(cursor, siteId, buyin, fee, knockout, rebuyOrAddon):
 # then this can be reduced in complexity a bit
 
 def recognisePlayerIDs(cursor, names, site_id):
-    cursor.execute("SELECT name,id FROM Players WHERE name='%s'" % "' OR name='".join(names)) # get all playerids by the names passed in
+    q = "SELECT name,id FROM Players WHERE name=%s" % " OR name=".join(["%s" for n in names])
+    cursor.execute(q, names) # get all playerids by the names passed in
     ids = dict(cursor.fetchall()) # convert to dict
     if len(ids) != len(names):
         notfound = [n for n in names if n not in ids] # make list of names not in database
         if notfound: # insert them into database
             cursor.executemany("INSERT INTO Players (name, siteId) VALUES (%s, "+str(site_id)+")", (notfound))
-            cursor.execute("SELECT name,id FROM Players WHERE name='%s'" % "' OR name='".join(notfound)) # get their new ids
+            q2 = "SELECT name,id FROM Players WHERE name=%s" % " OR name=".join(["%s" for n in notfound])
+            cursor.execute(q2, notfound) # get their new ids
             tmp = dict(cursor.fetchall())
             for n in tmp: # put them all into the same dict
                 ids[n] = tmp[n]
