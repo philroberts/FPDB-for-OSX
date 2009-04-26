326f29b5b607e253368359a548f98f3c8edc2abd
sqlcoder <sqlcoder@buylow.plus.com>
2009-04-26 21:47:28 +0100

    always release locks when db work finished

--- pyfpdb/fpdb_import.py
+++ pyfpdb/fpdb_import.py
@@ -74,6 +74,7 @@ class Importer:
         
         self.fdb = fpdb_db.fpdb_db()   # sets self.fdb.db self.fdb.cursor and self.fdb.sql
         self.fdb.do_connect(self.config)
+        self.fdb.db.rollback()
 
     #Set functions
     def setCallHud(self, value):
@@ -348,6 +349,7 @@ class Importer:
                             self.caller.pipe_to_hud.stdin.write("%s" % (handsId) + os.linesep)
                     except fpdb_simple.DuplicateError:
                         duplicates += 1
+                        self.fdb.db.rollback()
                     except (ValueError), fe:
                         errors += 1
                         self.printEmailErrorMessage(errors, file, hand)
@@ -355,6 +357,8 @@ class Importer:
                         if (self.settings['failOnError']):
                             self.fdb.db.commit() #dont remove this, in case hand processing was cancelled.
                             raise
+                        else:
+                            self.fdb.db.rollback()
                     except (fpdb_simple.FpdbError), fe:
                         errors += 1
                         self.printEmailErrorMessage(errors, file, hand)
