d2c978d7f59338199265e41475922df44499adc2
sqlcoder <sqlcoder@buylow.plus.com>
2009-04-26 22:20:24 +0100

    steal stat fixes to match pokertracker

--- pyfpdb/fpdb_simple.py
+++ pyfpdb/fpdb_simple.py
@@ -1630,6 +1630,11 @@ sure to also change the following storage method and table_viewer.prepare_data i
             firstPfCallByNo = i
             firstPfCallerId = action[0]
             break
+    firstPlayId = firstPfCallerId
+    if firstPfRaiseByNo <> -1:
+        if firstPfRaiseByNo < firstPfCallByNo or firstPfCallByNo == -1:
+            firstPlayId = firstPfRaiserId
+
     
     cutoffId=-1
     buttonId=-1
@@ -1702,25 +1707,28 @@ sure to also change the following storage method and table_viewer.prepare_data i
                     myStreet0_3B4BDone = True
         
         #steal calculations
-        if base == "hold":
-            if len(player_ids)>=5: #no point otherwise
+        if base=="hold":
+            if len(player_ids)>=3: # no point otherwise  # was 5, use 3 to match pokertracker definition
                 if positions[player]==1:
-                    if firstPfRaiserId==player_ids[player]:
-                        myStealAttemptChance=True
+                    if      firstPfRaiserId==player_ids[player] \
+                       and (firstPfCallByNo==-1 or firstPfCallByNo>firstPfRaiseByNo):
                         myStealAttempted=True
-                    elif firstPfRaiserId==buttonId or firstPfRaiserId==sbId or firstPfRaiserId==bbId or firstPfRaiserId==-1:
+                        myStealAttemptChance=True
+                    if firstPlayId==cutoffId or firstPlayId==buttonId or firstPlayId==sbId or firstPlayId==bbId or firstPlayId==-1:
                         myStealAttemptChance=True
                 if positions[player]==0:
-                    if firstPfRaiserId==player_ids[player]:
-                        myStealAttemptChance=True
+                    if      firstPfRaiserId==player_ids[player] \
+                       and (firstPfCallByNo==-1 or firstPfCallByNo>firstPfRaiseByNo):
                         myStealAttempted=True
-                    elif firstPfRaiserId==sbId or firstPfRaiserId==bbId or firstPfRaiserId==-1:
+                        myStealAttemptChance=True
+                    if firstPlayId==buttonId or firstPlayId==sbId or firstPlayId==bbId or firstPlayId==-1:
                         myStealAttemptChance=True
                 if positions[player]=='S':
-                    if firstPfRaiserId==player_ids[player]:
-                        myStealAttemptChance=True
+                    if      firstPfRaiserId==player_ids[player] \
+                       and (firstPfCallByNo==-1 or firstPfCallByNo>firstPfRaiseByNo):
                         myStealAttempted=True
-                    elif firstPfRaiserId==bbId or firstPfRaiserId==-1:
+                        myStealAttemptChance=True
+                    if firstPlayId==sbId or firstPlayId==bbId or firstPlayId==-1:
                         myStealAttemptChance=True
                 if positions[player]=='B':
                     pass
