c4d3789657b92bc876ff69f69b323ee2bae6781f
grindi <email-is-hidden>
2009-11-06 23:47:31 +0300

    Fixed: aux save layout doesn't work
    Bug maker, look at
    http://docs.python.org/tutorial/classes.html#generator-expressions
    (...for...) returns generator rather than tuple

--- pyfpdb/Hud.py
+++ pyfpdb/Hud.py
@@ -440,7 +440,7 @@ class Hud:
             new_layout[self.stat_windows[sw].adj - 1] = new_loc
         self.config.edit_layout(self.table.site, self.max, locations = new_layout)
 #    ask each aux to save its layout back to the config object
-        (aux.save_layout() for aux in self.aux_windows)
+        [aux.save_layout() for aux in self.aux_windows]
 #    save the config object back to the file
         print "saving new xml file"
         self.config.save()
