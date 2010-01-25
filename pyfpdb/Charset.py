eebec05cf8aa9739c95590b14f9cb1247def48d7
Mika Bostrom <bostik@odin.bostik.iki.fi>
2010-01-25 19:04:56 +0200

    Fix dual function def

--- pyfpdb/Charset.py
+++ pyfpdb/Charset.py
@@ -50,16 +50,6 @@ def to_db_utf8(s):
         (_out, _len) = encoder_to_utf.encode(unicode(s))
         return _out
     except UnicodeDecodeError:
-        sys.stderr.write('Could not convert: "%s"\n' % s)
-        raise
-
-def to_db_utf8(s):
-    if not_needed: return s
-
-    try:
-        (_out, _len) = encoder_to_utf.encode(unicode(s))
-        return _out
-    except UnicodeDecodeError:
         print 'Could not convert: "%s"' % s
         raise
 
