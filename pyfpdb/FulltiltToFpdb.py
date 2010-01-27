929e182a28ecd163accbab6d615d25b504ca9f64
Worros <carl.gherardi@gmail.com>
2010-01-27 18:20:48 +0800

    FulltiltToFpdb: Fix tourney player matching for players sitting out

--- pyfpdb/FulltiltToFpdb.py
+++ pyfpdb/FulltiltToFpdb.py
@@ -68,7 +68,7 @@ class Fulltilt(HandHistoryConverter):
                                     ''', re.VERBOSE)
     re_Button       = re.compile('^The button is in seat #(?P<BUTTON>\d+)', re.MULTILINE)
     re_PlayerInfo   = re.compile('Seat (?P<SEAT>[0-9]+): (?P<PNAME>.*) \(\$(?P<CASH>[,.0-9]+)\)$', re.MULTILINE)
-    re_TourneyPlayerInfo   = re.compile('Seat (?P<SEAT>[0-9]+): (?P<PNAME>.*) \(\$?(?P<CASH>[,.0-9]+)\)$', re.MULTILINE)
+    re_TourneyPlayerInfo   = re.compile('Seat (?P<SEAT>[0-9]+): (?P<PNAME>.*) \(\$?(?P<CASH>[,.0-9]+)\)(, is sitting out)?$', re.MULTILINE)
     re_Board        = re.compile(r"\[(?P<CARDS>.+)\]")
 
     #static regex for tourney purpose
