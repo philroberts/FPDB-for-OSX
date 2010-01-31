cc5f77e950421dbd708e1e9ba773fc91784a6bf7
sqlcoder <sqlcoder@buylow.plus.com>
2010-01-31 11:16:42 +0000

    fix windows problem starting hud

--- pyfpdb/GuiAutoImport.py
+++ pyfpdb/GuiAutoImport.py
@@ -194,7 +194,8 @@ class GuiAutoImport (threading.Thread):
                 widget.set_label(u'  _Stop Autoimport  ')
                 if self.pipe_to_hud is None:
                     if os.name == 'nt':
-                        command = "python "+sys.path[0]+"\\HUD_main.py " + self.settings['cl_options']
+                        path = sys.path[0].replace('\\','\\\\')
+                        command = 'python "'+path+'\\HUD_main.py" ' + self.settings['cl_options']
                         bs = 0
                     else:
                         command = os.path.join(sys.path[0], 'HUD_main.py')
