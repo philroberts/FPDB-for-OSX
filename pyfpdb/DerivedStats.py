6814538594bb56faaf7d0234fae4ea426d5e90a0
sqlcoder <sqlcoder@buylow.plus.com>
2010-02-14 12:50:44 +0000

    a wtsd fix, but figures looks further out if anything now. see riterman in FLHE_6max_sawflop_wrong.txt for an example where street1seen is still not set

--- pyfpdb/DerivedStats.py
+++ pyfpdb/DerivedStats.py
@@ -264,6 +264,10 @@ class DerivedStats():
         if self.hands['playersAtShowdown'] > 1:
             for player in pas:
                 self.handsplayers[player]['sawShowdown'] = True
+                # streetXseenFix: streetXseen values miss out streets that were checked through,
+                # make sure all are set if showdown is seen
+                for i in xrange(1, len(hand.actionStreets)-1):
+                    self.handsplayers[player]['street%sSeen' % i] = True
 
     def streetXRaises(self, hand):
         # self.actions[street] is a list of all actions in a tuple, contining the action as the second element
@@ -375,7 +379,10 @@ class DerivedStats():
 
         for player in hand.players:
             if player[1] in pas:
-                self.handsplayers[player[1]]['street%sSeen' % i] = True
+                # streetXseenFix: streetXseen values miss out streets that were checked through,
+                # make sure earlier ones are set if action on later street
+                for j in xrange(1,i+1):
+                    self.handsplayers[player[1]]['street%sSeen' % j] = True
             else:
                 self.handsplayers[player[1]]['street%sSeen' % i] = False
 
