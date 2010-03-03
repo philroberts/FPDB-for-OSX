17b02c1cd9cc5939e96f9ee2dcdec586c2f48e05
Eratosthenes <ray.barker@hughes.net>
2010-03-03 12:57:30 -0500

    Fix for missing TTYPE in HandInfo.

--- pyfpdb/PartyPokerToFpdb.py
+++ pyfpdb/PartyPokerToFpdb.py
@@ -77,7 +77,7 @@ class PartyPoker(HandHistoryConverter):
           re.VERBOSE)
 
     re_HandInfo     = re.compile("""
-            ^Table\s+(?P<TTYPE>[$a-zA-Z0-9 ]+)\s+
+            ^Table\s+(?P<TTYPE>[$a-zA-Z0-9 ]+)?\s+
             (?: \#|\(|)(?P<TABLE>\d+)\)?\s+
             (?:[a-zA-Z0-9 ]+\s+\#(?P<MTTTABLE>\d+).+)?
             (\(No\sDP\)\s)?
