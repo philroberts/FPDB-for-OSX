acebb0c4035221f25e648f0b71f001379689c146
Gerko de Roo <gerko.deroo@smurfette.nl>
2010-03-04 14:11:50 +0100

    Player stat fix for PartyPoker
    When placing a uncalled bet (like all-in) the uncalled part is put in a sitepot
    Therefore the player exist 2* in collected.
    The uncalled part of the bet was subtracted twice for collected and collectees.

--- pyfpdb/PartyPokerToFpdb.py
+++ pyfpdb/PartyPokerToFpdb.py
@@ -261,6 +261,7 @@ class PartyPoker(HandHistoryConverter):
                     if v[0] in self.pot.returned:
                         self.collected[i][1] = Decimal(v[1]) - self.pot.returned[v[0]]
                         self.collectees[v[0]] -= self.pot.returned[v[0]]
+                        self.pot.returned[v[0]] = 0
                 return origTotalPot()
             return totalPot
         instancemethod = type(hand.totalPot)
