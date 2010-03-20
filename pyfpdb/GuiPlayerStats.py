a478331988333623600cd9dc55c25cbef88ae7cc
sqlcoder <sqlcoder@buylow.plus.com>
2010-03-20 18:20:40 +0000

    change button name to differentiate from filter refresh

--- pyfpdb/GuiPlayerStats.py
+++ pyfpdb/GuiPlayerStats.py
@@ -81,7 +81,7 @@ class GuiPlayerStats (threading.Thread):
         self.filters = Filters.Filters(self.db, self.conf, self.sql, display = filters_display)
         self.filters.registerButton1Name("_Filters")
         self.filters.registerButton1Callback(self.showDetailFilter)
-        self.filters.registerButton2Name("_Refresh")
+        self.filters.registerButton2Name("_Refresh Stats")
         self.filters.registerButton2Callback(self.refreshStats)
 
         # ToDo: store in config
