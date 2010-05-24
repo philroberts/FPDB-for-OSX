9964475dff77b299112676d8c294fd1c596d003e
Mika Bostrom <bostik@odin.bostik.iki.fi>
2010-05-24 18:41:40 +0300

    Always use single statline generation
    Do not produce different stat lines/calculations based on whether the
    name string is postfixed with "_N" where N is number of decimal places.

--- pyfpdb/Stats.py
+++ pyfpdb/Stats.py
@@ -78,11 +78,12 @@ def do_stat(stat_dict, player = 24, stat = 'vpip'):
         base = stat[0:-2]
         places = int(stat[-1:])
         result = eval("%(stat)s(stat_dict, %(player)d)" % {'stat': base, 'player': player})
-        match = re_Percent.search(result[1])
-        if match is None:
-            result = (result[0], "%.*f" % (places, result[0]), result[2], result[3], result[4], result[5])
-        else:
-            result = (result[0], "%.*f%%" % (places, 100*result[0]), result[2], result[3], result[4], result[5])
+        #match = re_Percent.search(result[1])
+        #if match is None:
+        #    result = (result[0], "%.*f" % (places, 100*result[0]), result[2], result[3], result[4], result[5])
+        #else:
+        #    result = (result[0], "%.*f%%" % (places, 100*result[0]), result[2], result[3], result[4], result[5])
+        result = (result[0], "%.*f" % (places, 100*result[0]), result[2], result[3], result[4], result[5])
     return result
 
 #    OK, for reference the tuple returned by the stat is:
