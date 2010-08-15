aacfb61d3b20ebe0858177e87a6e2f7562f3b2a9
steffen123 <steffen@schaumburger.info>
2010-08-15 23:23:17 +0200

    add missing gettext imports, gettextify TournamentTracker.py

--- pyfpdb/Hand.py
+++ pyfpdb/Hand.py
@@ -28,6 +28,18 @@ import time,datetime
 from copy import deepcopy
 import pprint
 
+import locale
+lang=locale.getdefaultlocale()[0][0:2]
+if lang=="en":
+    def _(string): return string
+else:
+    import gettext
+    try:
+        trans = gettext.translation("fpdb", localedir="locale", languages=[lang])
+        trans.install()
+    except IOError:
+        def _(string): return string
+
 import logging
 # logging has been set up in fpdb.py or HUD_main.py, use their settings:
 log = logging.getLogger("parser")
