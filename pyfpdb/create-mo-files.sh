027afaa5e10aca022755fa4232a371440c974979
steffen123 <steffen@schaumburger.info>
2010-08-15 23:28:22 +0200

    change mo file creation script, add hungarian mo file

--- pyfpdb/create-mo-files.sh
+++ pyfpdb/create-mo-files.sh
@@ -1,1 +1,1 @@
-python /usr/share/doc/python-2.7/examples/Tools/i18n/msgfmt.py --output-file=locale/de_DE/LC_MESSAGES/fpdb.mo locale/fpdb-de_DE.po
+python /usr/share/doc/python-2.7/examples/Tools/i18n/msgfmt.py --output-file=locale/hu/LC_MESSAGES/fpdb.mo locale/fpdb-hu_HU.po
