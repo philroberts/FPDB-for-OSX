4fcc157ff854f07d20b2308e932a2149afc4e84a
steffen123 <steffen@schaumburger.info>
2010-08-22 07:25:07 +0200

    update empty and 0001 dump for DB v143

--- pyfpdb/regression-test-files/cash/Stars/Flop/LHE-10max-USD-0.05-0.10-PF_all_fold_to_BB.txt.0001.sql
+++ pyfpdb/regression-test-files/cash/Stars/Flop/LHE-10max-USD-0.05-0.10-PF_all_fold_to_BB.txt.0001.sql
@@ -1,5 +1,5 @@
 fpdb database dump
-DB version=142
+DB version=143
 
 ###################
 Table Autorates
@@ -1643,11 +1643,20 @@ Table Players
   comment=None
   commentTs=None
 
+###################
+Table RawHands
+###################
+empty table
+
+###################
+Table RawTourneys
+###################
+empty table
 
 ###################
 Table Settings
 ###################
-  version=142
+  version=143
 
 
 ###################
