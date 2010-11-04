bcc97636a6b09806c3821f3499761fd405d80efa
Worros <carl.gherardi@gmail.com>
2010-11-04 19:59:16 +0800

    Stars: Fix CAP lines where the cap is reached

--- pyfpdb/PokerStarsToFpdb.py
+++ pyfpdb/PokerStarsToFpdb.py
@@ -147,6 +147,7 @@ class PokerStars(HandHistoryConverter):
                         ^%(PLYR)s:(?P<ATYPE>\sbets|\schecks|\sraises|\scalls|\sfolds|\sdiscards|\sstands\spat)
                         (\s(%(CUR)s)?(?P<BET>[.\d]+))?(\sto\s%(CUR)s(?P<BETTO>[.\d]+))?  # the number discarded goes in <BET>
                         \s*(and\sis\sall.in)?
+                        (and\shas\sreached\sthe\s[%(CUR)s\d\.]+\scap)?
                         (\scards?(\s\[(?P<DISCARDED>.+?)\])?)?\s*$"""
                          %  subst, re.MULTILINE|re.VERBOSE)
             self.re_ShowdownAction   = re.compile(r"^%s: shows \[(?P<CARDS>.*)\]" %  player_re, re.MULTILINE)
