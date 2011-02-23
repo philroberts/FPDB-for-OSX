d30fb9e72ca9c3e38728694439f2af6a61407431
Eratosthenes <ray.barker@hughes.net>
2011-02-23 11:59:59 -0500

    Catch match error on new tables retry or fail gracefully.

--- pyfpdb/XTables.py
+++ pyfpdb/XTables.py
@@ -67,11 +67,11 @@ class Table(Table_Window):
                 self.number = int( mo.groupdict()["XID"], 0 )
                 self.title = title
                 if self.number is None:
-                    log.error("Could not retrieve XID from table xwininfo. xwininfo is %s" % listing)
+                    log.warning("Could not retrieve XID from table xwininfo. xwininfo is %s" % listing)
                 break
 
         if self.number is None:
-            log.error("No match in XTables for table '%s'." % self.search_string)
+            log.warning("No match in XTables for table '%s'." % self.search_string)
             return None
         (self.window, self.parent) = self.get_window_from_xid(self.number)
 
