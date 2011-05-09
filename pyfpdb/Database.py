744336ae7298e6c7fb1fe182d7cd5f0aea9f7d29
Worros <carl.gherardi@gmail.com>
2011-05-09 18:47:48 +0800

    Database: Add Cake

--- pyfpdb/Database.py
+++ pyfpdb/Database.py
@@ -1559,6 +1559,7 @@ class Database:
         c.execute("INSERT INTO Sites (id,name,code) VALUES ('14', 'iPoker', 'IP')")
         c.execute("INSERT INTO Sites (id,name,code) VALUES ('15', 'Winamax', 'WM')")
         c.execute("INSERT INTO Sites (id,name,code) VALUES ('16', 'Everest', 'EP')")
+        c.execute("INSERT INTO Sites (id,name,code) VALUES ('17', 'Cake', 'CK')")
         #Fill Actions
         c.execute("INSERT INTO Actions (id,name,code) VALUES ('1', 'ante', 'A')")
         c.execute("INSERT INTO Actions (id,name,code) VALUES ('2', 'small blind', 'SB')")
