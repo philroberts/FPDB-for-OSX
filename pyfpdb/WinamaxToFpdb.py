d028899cf35a1cfcdfb998d1301d95d517616280
Steffen Schaumburg <steffen@schaumburger.info>
2011-05-10 01:44:36 +0200

    fix recording fo sitehandno in winamax

--- pyfpdb/WinamaxToFpdb.py
+++ pyfpdb/WinamaxToFpdb.py
@@ -221,7 +221,7 @@ class Winamax(HandHistoryConverter):
             if key == 'HID1':
                 # Need to remove non-alphanumerics for MySQL
 #                hand.handid = "1%.9d%s%s"%(int(info['HID2']),info['HID1'],info['HID3'])
-                hand.handid = "%s%s%s"%(int(info['HID2']),info['HID1'],info['HID3'])
+                hand.handid = "%s%s%s"%(int(info['HID1']),info['HID2'],info['HID3'])
                 if len (hand.handid) > 19:
                     hand.handid = "%s%s" % (int(info['HID2']), int(info['HID3']))
                     
