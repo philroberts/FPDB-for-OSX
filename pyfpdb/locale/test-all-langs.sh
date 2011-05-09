84661d801206f986d863f1fc738a8d5e186920a4
Steffen Schaumburg <steffen@schaumburger.info>
2011-05-09 20:42:58 +0200

    add script to start fpdb

---
+++ pyfpdb/locale/test-all-langs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+LC_ALL="de_DE.UTF-8" ../../run_fpdb.py 
+LC_ALL="es_ES.UTF-8" ../../run_fpdb.py 
+LC_ALL="fr_FR.UTF-8" ../../run_fpdb.py 
+LC_ALL="hu_HU.UTF-8" ../../run_fpdb.py 
+LC_ALL="it_IT.UTF-8" ../../run_fpdb.py 
+LC_ALL="pl_PL.UTF-8" ../../run_fpdb.py 
+LC_ALL="pt_BR.UTF-8" ../../run_fpdb.py 
+LC_ALL="ro_RO.UTF-8" ../../run_fpdb.py 
+LC_ALL="ru_RU.UTF-8" ../../run_fpdb.py 
+LC_ALL="zh_CN.UTF-8" ../../run_fpdb.py 
+
