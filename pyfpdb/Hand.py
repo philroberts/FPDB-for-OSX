912da44bb58fbe41b399f19ab7849dc25f893ce9
Chris Moore <dooglus@gmail.com>
2011-05-21 18:04:27 -0700

    Carbon Poker offers a 5 card stud game that wasn't listed here.  It's not available on PokerStars (yet?), so there's no "pokerstars string describing it".

--- pyfpdb/Hand.py
+++ pyfpdb/Hand.py
@@ -767,6 +767,7 @@ Map the tuple self.gametype onto the pokerstars string describing it
               "fivedraw"   : "5 Card Draw",
               "27_1draw"   : "Single Draw 2-7 Lowball",
               "27_3draw"   : "Triple Draw 2-7 Lowball",
+              "5studhi"    : "5 Card Stud",
               "badugi"     : "Badugi"
              }
         ls = {"nl"  : "No Limit",
