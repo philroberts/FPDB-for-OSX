e857aa8f6a1a963f3076e8545e1dde3d0e1c57ac
Carl Gherardi <carl.gherardi@gmail.com>
2011-09-12 11:06:49 +0800

    Card: Fix lookup table - was missing JTo

--- pyfpdb/Card.py
+++ pyfpdb/Card.py
@@ -30,7 +30,7 @@ card_map_abbr = [
                  ["AKo","KK" ,"KQs","KJs","KTs","K9s","K8s","K7s","K6s","K5s","K4s","K3s","K2s"],
                  ["AQo","KQo","QQ" ,"QJs","QTs","Q9s","Q8s","Q7s","Q6s","Q5s","Q4s","Q3s","Q2s"],
                  ["AJo","KJo","QJo","JJ" ,"JTs","J9s","J8s","J7s","J6s","J5s","J4s","J3s","J2s"],
-                 ["ATo","KTo","QTo","QJo","TT" ,"T9s","T8s","T7s","T6s","T5s","T4s","T3s","T2s"],
+                 ["ATo","KTo","QTo","JTo","TT" ,"T9s","T8s","T7s","T6s","T5s","T4s","T3s","T2s"],
                  ["A9o","K9o","Q9o","J9o","T9o","99" ,"98s","97s","96s","95s","94s","93s","92s"],
                  ["A8o","K8o","Q8o","J8o","T8o","98o","88" ,"87s","86s","85s","84s","83s","82s"],
                  ["A7o","K7o","Q7o","J7o","T7o","97o","87o","77" ,"76s","75s","74s","73s","72s"],
