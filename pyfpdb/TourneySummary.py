4eaa40d8ee56cb9e6e9a5fe8d0e7db5502e5a182
Carl Gherardi <carl.gherardi@gmail.com>
2011-12-15 11:43:51 +0800

    iPokerSummary: Config

--- pyfpdb/TourneySummary.py
+++ pyfpdb/TourneySummary.py
@@ -49,7 +49,10 @@ class TourneySummary(object):
     LCS = {'H':'h', 'D':'d', 'C':'c', 'S':'s'}                                                  # SAL- TO KEEP ??
     SYMBOL = {'USD': '$', 'EUR': u'$', 'T$': '', 'play': ''}
     MS = {'horse' : 'HORSE', '8game' : '8-Game', 'hose'  : 'HOSE', 'ha': 'HA'}
-    SITEIDS = {'Fulltilt':1, 'Full Tilt Poker':1, 'PokerStars':2, 'Everleaf':3, 'Boss':4, 'OnGame':5, 'UltimateBet':6, 'Betfair':7, 'Absolute':8, 'PartyPoker':9, 'Merge':11, 'Winamax':14 }
+    SITEIDS = {'Fulltilt':1, 'Full Tilt Poker':1, 'PokerStars':2, 'Everleaf':3, 'Boss':4, 'OnGame':5,
+               'UltimateBet':6, 'Betfair':7, 'Absolute':8, 'PartyPoker':9, 'PacificPoker':10,
+               'Partouche':11, 'Merge':12, 'PKR':13, 'iPoker':14, 'Winamax':15,
+               'Everest':16, 'Cake':17, 'Entraction':18, 'BetOnline':19, 'Microgaming':20 }
 
 
     def __init__(self, db, config, siteName, summaryText, builtFrom = "HHC"):
