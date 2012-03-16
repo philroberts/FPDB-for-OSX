9a0247fc03613ba18d5567eabef2e798696bb082
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-03-16 07:42:39 +0000

    Cleaned up the FTP tourney summary regex a bit and added support for the use of K and M to represent 1,000s and 1,000,000s in FTP tournaments

--- pyfpdb/HandHistoryConverter.py
+++ pyfpdb/HandHistoryConverter.py
@@ -591,6 +591,12 @@ or None if we fail to get the info """
         if not money:
             return money
         money = money.replace(' ', '')
+        if 'K' in money:
+            money = money.replace('K', '')
+            money += '000'
+        if 'M' in money:
+            money = money.replace('M', '')
+            money += '000000'
         if len(money) < 3:
             return money # No commas until 0,01 or 1,00
         if money[-3] == ',':
