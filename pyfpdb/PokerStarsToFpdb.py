9cae17a15a41b9f88b77b44f89768f9adff3dc34
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-04-21 19:26:56 +0000

    Added support for Mixed NLH/PLO in PokerStarsToFpdb, and ported over all the PokerStars mix games to PokerStarsSummary

--- pyfpdb/PokerStarsToFpdb.py
+++ pyfpdb/PokerStarsToFpdb.py
@@ -95,6 +95,7 @@ class PokerStars(HandHistoryConverter):
                                 '8-GAME': '8game',
                                   'HOSE': 'hose',
                          'Mixed PLH/PLO': 'plh_plo',
+                         'Mixed NLH/PLO': 'nlh_plo',
                        'Mixed Omaha H/L': 'plo_lo',
                         'Mixed Hold\'em': 'mholdem',
                            'Triple Stud': '3stud'
@@ -109,7 +110,7 @@ class PokerStars(HandHistoryConverter):
           # here's how I plan to use LS
           (?P<BUYIN>(?P<BIAMT>[%(LS)s\d\.]+)?\+?(?P<BIRAKE>[%(LS)s\d\.]+)?\+?(?P<BOUNTY>[%(LS)s\d\.]+)?\s?(?P<TOUR_ISO>%(LEGAL_ISO)s)?|Freeroll)\s+)?
           # close paren of tournament info
-          (?P<MIXED>HORSE|8\-Game|8\-GAME|HOSE|Mixed\sOmaha\sH/L|Mixed\sHold\'em|Mixed\sPLH/PLO|Triple\sStud)?\s?\(?
+          (?P<MIXED>HORSE|8\-Game|8\-GAME|HOSE|Mixed\sOmaha\sH/L|Mixed\sHold\'em|Mixed\sPLH/PLO|Mixed\sNLH/PLO|Triple\sStud)?\s?\(?
           (?P<GAME>Hold\'em|HOLD\'EM|Razz|RAZZ|7\sCard\sStud|7\sCARD\sSTUD|7\sCard\sStud\sHi/Lo|7\sCARD\sSTUD\sHI/LO|Omaha|OMAHA|Omaha\sHi/Lo|OMAHA\sHI/LO|Badugi|Triple\sDraw\s2\-7\sLowball|Single\sDraw\s2\-7\sLowball|5\sCard\sDraw)\s
           (?P<LIMIT>No\sLimit|NO\sLIMIT|Limit|LIMIT|Pot\sLimit|POT\sLIMIT)\)?,?\s
           (-\s)?
