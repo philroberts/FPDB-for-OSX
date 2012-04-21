8bdf31d6f7feaba405e4552ae852f988a67ad0ea
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-04-21 19:34:25 +0000

    Added 'post both' support to PokerTrackerToFpdb as well as an occasionally used addRaiseBy method

--- pyfpdb/PokerTrackerToFpdb.py
+++ pyfpdb/PokerTrackerToFpdb.py
@@ -150,7 +150,7 @@ class PokerTracker(HandHistoryConverter):
     re_HeroCards        = re.compile(r"^Dealt to %(PLYR)s(?: \[(?P<OLDCARDS>.+?)\])?( \[(?P<NEWCARDS>.+?)\])" % substitutions, re.MULTILINE)
     re_Action           = re.compile(r"""
                         ^%(PLYR)s:?(?P<ATYPE>\sbets|\schecks|\sraises|\scalls|\sfolds|\sBet|\sCheck|\sRaise|\sCall|\sFold|\sAllin)
-                        (\s\(NF\))?
+                        (?P<RAISETO>\s\(NF\))?
                         (\sto)?(\s%(CUR)s(?P<BET>[%(NUM)s]+))?  # the number discarded goes in <BET>
                         \s*(and\sis\sall.in)?
                         (and\shas\sreached\sthe\s[%(CUR)s\d\.,]+\scap)?
@@ -405,9 +405,15 @@ class PokerTracker(HandHistoryConverter):
                 hand.addBlind(a.group('PNAME'), 'secondsb', a.group('SB'))
         for a in self.re_PostBB.finditer(hand.handText):
             self.adjustMergeTourneyStack(hand, a.group('PNAME'), a.group('BB'))
-            hand.addBlind(a.group('PNAME'), 'big blind', a.group('BB'))
             if not hand.gametype['bb']:
                 hand.gametype['bb'] = self.clearMoneyString(a.group('BB'))
+                hand.addBlind(a.group('PNAME'), 'big blind', a.group('BB'))
+            else:
+                both = Decimal(hand.gametype['bb']) + Decimal(hand.gametype['bb'])/2
+                if both == Decimal(a.group('BB')):
+                    hand.addBlind(a.group('PNAME'), 'big blind', a.group('BB'))
+                else:
+                    hand.addBlind(a.group('PNAME'), 'both', a.group('BB'))
         for a in self.re_PostBoth.finditer(hand.handText):
             self.adjustMergeTourneyStack(hand, a.group('PNAME'), a.group('SBBB'))
             hand.addBlind(a.group('PNAME'), 'both', a.group('SBBB'))
@@ -471,6 +477,7 @@ class PokerTracker(HandHistoryConverter):
 
     def readAction(self, hand, street):
         m = self.re_Action.finditer(hand.streets[street])
+        curr_pot = Decimal('0')
         for action in m:
             acts = action.groupdict()
             #print "DEBUG: acts: %s" %acts
@@ -481,7 +488,14 @@ class PokerTracker(HandHistoryConverter):
             elif action.group('ATYPE') in (' calls', ' Call'):
                 hand.addCall( street, action.group('PNAME'), action.group('BET') )
             elif action.group('ATYPE') in (' raises', ' Raise'):
-                hand.addRaiseTo( street, action.group('PNAME'), action.group('BET') )
+                amount = Decimal(action.group('BET'))
+                if curr_pot > amount:
+                    hand.addCall( street, action.group('PNAME'), action.group('BET') )
+                elif not action.group('RAISETO') and action.group('ATYPE')==' Raise':
+                    hand.addRaiseBy( street, action.group('PNAME'), action.group('BET') )
+                else:
+                    hand.addRaiseTo( street, action.group('PNAME'), action.group('BET') )
+                curr_pot = amount
             elif action.group('ATYPE') in (' bets', ' Bet'):
                 hand.addBet( street, action.group('PNAME'), action.group('BET') )
             elif action.group('ATYPE') == ' Allin':
