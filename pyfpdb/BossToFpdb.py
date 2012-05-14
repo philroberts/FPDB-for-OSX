446b2ed855238d8ed5b689e5eb1e0653b91d5fc8
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-05-14 19:33:25 +0000

    Fixed a bug here as hand.bets['BLINDSANTES'] contains lists

--- pyfpdb/BossToFpdb.py
+++ pyfpdb/BossToFpdb.py
@@ -366,13 +366,13 @@ class Boss(HandHistoryConverter):
                 bet = action.group('BET')
                 if street in ('PREFLOP', 'DEAL'):
                     blind = hand.bets['BLINDSANTES'].get(action.group('PNAME'))
-                    if blind: bet = str(Decimal(action.group('BET')) - blind)
+                    if blind: bet = str(Decimal(action.group('BET')) - blind[0])
                 hand.addCallTo(street, action.group('PNAME'), bet )
             elif action.group('ATYPE') == 'ACTION_RAISE':
                 bet = action.group('BET')
                 if street in ('PREFLOP', 'DEAL'):
                     blind = hand.bets['BLINDSANTES'].get(action.group('PNAME'))
-                    if blind: bet = str(Decimal(action.group('BET')) - blind)
+                    if blind: bet = str(Decimal(action.group('BET')) - blind[0])
                 hand.addRaiseTo( street, action.group('PNAME'), action.group('BET') )
             elif action.group('ATYPE') == 'ACTION_BET':
                 hand.addBet( street, action.group('PNAME'), action.group('BET') )
@@ -384,7 +384,7 @@ class Boss(HandHistoryConverter):
                 bet = action.group('BET')
                 if street in ('PREFLOP', 'DEAL'):
                     blind = hand.bets['BLINDSANTES'].get(action.group('PNAME'))
-                    if blind: bet = str(Decimal(action.group('BET')) - blind)
+                    if blind: bet = str(Decimal(action.group('BET')) - blind[0])
                 hand.addRaiseTo( street, action.group('PNAME'), action.group('BET') )
             else:
                 print (_("DEBUG:") + _("Unimplemented %s: '%s' '%s'") % ("readAction", action.group('PNAME'), action.group('ATYPE')))
