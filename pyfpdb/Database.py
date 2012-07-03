6ec9cf6fa27aed1defb53c8fa705d9aee4a510a9
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-07-03 04:47:38 +0000

    New Site Bovada/Bodog and new database version 174. Currently supports flop games and stud for cash.
    Conflicts:
    	pyfpdb/Database.py
    	pyfpdb/HUD_config.xml.example
    	pyfpdb/IdentifySite.py

--- pyfpdb/Database.py
+++ pyfpdb/Database.py
@@ -1520,6 +1520,7 @@ class Database:
         c.execute("INSERT INTO Sites (id,name,code) VALUES ('18', 'Entraction', 'TR')")
         c.execute("INSERT INTO Sites (id,name,code) VALUES ('19', 'BetOnline', 'BO')")
         c.execute("INSERT INTO Sites (id,name,code) VALUES ('20', 'Microgaming', 'MG')")
+        c.execute("INSERT INTO Sites (id,name,code) VALUES ('21', 'Bovada', 'BV')")
         #Fill Actions
         c.execute("INSERT INTO Actions (id,name,code) VALUES ('1', 'ante', 'A')")
         c.execute("INSERT INTO Actions (id,name,code) VALUES ('2', 'small blind', 'SB')")
