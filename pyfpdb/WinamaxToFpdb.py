73bce27e974bb4d5e90703055ae69eeb6c571b4b
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-08-23 00:21:20 +0000

    In contrast to other sites, an all-in big blind is treated as the 'last bet' even if it is lower in value than the small blind.

--- pyfpdb/WinamaxToFpdb.py
+++ pyfpdb/WinamaxToFpdb.py
@@ -358,6 +358,8 @@ class Winamax(HandHistoryConverter):
             #hand.addBlind(None, None, None)
         for a in self.re_PostBB.finditer(hand.handText):
             hand.addBlind(a.group('PNAME'), 'big blind', a.group('BB'))
+            amount = Decimal(a.group('BB').replace(u',', u''))
+            hand.lastBet['PREFLOP'] = amount
         for a in self.re_PostDead.finditer(hand.handText):
             #print "DEBUG: Found dead blind: addBlind(%s, 'secondsb', %s)" %(a.group('PNAME'), a.group('DEAD'))
             hand.addBlind(a.group('PNAME'), 'secondsb', a.group('DEAD'))
