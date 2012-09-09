92cfbc77ff5550d8a09efc2bd9171014981c259f
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-09-09 21:40:46 +0000

    Better way to double check whether uncalled bets should be removed from collected/collectees is to to check if the failure to do so would result in negative rake.

--- pyfpdb/Hand.py
+++ pyfpdb/Hand.py
@@ -808,22 +808,22 @@ class Hand(object):
             self.totalpot = self.pot.total
         
         def gettempcontainers():
-            (collected, collectees, valid) = ([], {}, True)
+            (collected, collectees, totalcolleted) = ([], {}, 0)
             for i,v in enumerate(self.collected):
+                totalcolleted += Decimal(v[1])
                 collected.append([v[0], Decimal(v[1])])
             for k, j in self.collectees.iteritems():
                 collectees[k] = j
-            return collected, collectees, valid
+            return collected, collectees, totalcolleted
         
         if self.uncalledbets:
-            collected, collectees, valid = gettempcontainers()
+            collected, collectees, totalcolleted = gettempcontainers()
             for i,v in enumerate(self.collected):
-                if v[0] in self.pot.returned:
+                if v[0] in self.pot.returned: 
                     collected[i][1] = Decimal(v[1]) - self.pot.returned[v[0]]
                     collectees[v[0]] -= self.pot.returned[v[0]]
                     self.pot.returned[v[0]] = 0
-                    if collectees[v[0]] < 0: valid = False            
-            if valid:
+            if self.totalpot - totalcolleted < 0:
                 (self.collected, self.collectees) = (collected, collectees)
 
         # This gives us the amount collected, i.e. after rake
