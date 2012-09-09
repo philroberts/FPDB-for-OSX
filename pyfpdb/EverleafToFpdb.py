71e59ce73e63b6203e08d8fbb4471884726f962f
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-09-09 21:38:51 +0000

    re_CollectPot now works with high/low games

--- pyfpdb/EverleafToFpdb.py
+++ pyfpdb/EverleafToFpdb.py
@@ -78,7 +78,7 @@ class Everleaf(HandHistoryConverter):
             # ^%s(?P<ATYPE>: bets| checks| raises| calls| folds)(\s\[(?:\$| €|) (?P<BET>[.,\d]+) (USD|EURO|EUR|Chips)\])?
             self.re_Action          = re.compile(ur"^%(PLAYERS)s(?P<ATYPE>: bets| checks| raises| calls| folds)(\s\[(?: ?[%(LS)s]?) (?P<BET>[%(NUM)s]+)\s?(USD|EURO|EUR|Chips|)\])?" % self.substitutions, re.MULTILINE)
             self.re_ShowdownAction  = re.compile(ur"^%(PLAYERS)s (?P<SHOWED>shows|mucks) \[ (?P<CARDS>.*) \] (?P<STRING>.*)" % self.substitutions, re.MULTILINE)
-            self.re_CollectPot      = re.compile(ur"^%(PLAYERS)s wins  ?(?: ?[%(LS)s]?)\s?(?P<POT>[%(NUM)s]+) (USD|EURO|EUR|chips)(.*?\[ (?P<CARDS>.*?) \])?" % self.substitutions, re.MULTILINE)
+            self.re_CollectPot      = re.compile(ur"^%(PLAYERS)s wins ( (high|low) )?\(?\s?[%(LS)s]?\s?(?P<POT>[%(NUM)s]+)\s?(USD|EURO|EUR|chips)?\)?" % self.substitutions, re.MULTILINE)
             self.re_SitsOut         = re.compile(ur"^%s sits out" % self.substitutions, re.MULTILINE)
 
     def readSupportedGames(self):
