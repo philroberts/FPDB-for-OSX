8fba385c06e3769cfc3c1dc4cbaefe8288db33d3
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-10-14 23:23:52 +0000

    Added dead stack support for BetOnline tour.

--- pyfpdb/BetOnlineToFpdb.py
+++ pyfpdb/BetOnlineToFpdb.py
@@ -41,7 +41,7 @@ class BetOnline(HandHistoryConverter):
     sym = {'USD': "\$", 'CAD': "\$", 'T$': "", "EUR": "\xe2\x82\xac", "GBP": "\xa3", "play": ""}         # ADD Euro, Sterling, etc HERE
     substitutions = {
                      'LS' : u"\$|\xe2\x82\xac|\u20ac|", # legal currency symbols - Euro(cp1252, utf-8)
-                     'PLYR': r'(?P<PNAME>.+?)',
+                     'PLYR': r'(?P<PNAME>.*)',
                      'NUM' :u".,\d",
                     }
                     
