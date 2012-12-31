48556d2b3ce5d990d7d35421bdef474d9695aadc
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2012-12-31 21:27:06 +0000

    Updated showdown format to support new sample file

--- pyfpdb/CakeToFpdb.py
+++ pyfpdb/CakeToFpdb.py
@@ -115,8 +115,8 @@ class Cake(HandHistoryConverter):
                          %  substitutions, re.MULTILINE|re.VERBOSE)
     re_sitsOut          = re.compile("^%s sits out" %  substitutions['PLYR'], re.MULTILINE)
     re_ShownCards       = re.compile(r"^%s: (?P<SHOWED>shows|mucks) \[(?P<CARDS>.*)\] (\((?P<STRING>.*)\))?" % substitutions['PLYR'], re.MULTILINE)
-    re_CollectPot       = re.compile(r"^%(PLYR)s wins %(CUR)s(?P<POT>[%(NUM)s]+)((\swith.+?)?\s+\(EUR\s(%(CUR)s)?(?P<EUROVALUE>[%(NUM)s]+)\))?" %  substitutions, re.MULTILINE)
-    re_Finished         = re.compile(r"%(PLYR)s finished \d+ out of \d+ players" %  substitutions, re.MULTILINE)
+    re_CollectPot       = re.compile(r"^%(PLYR)s:? wins %(CUR)s(?P<POT>[%(NUM)s]+)((\swith.+?)?\s+\(EUR\s(%(CUR)s)?(?P<EUROVALUE>[%(NUM)s]+)\))?" %  substitutions, re.MULTILINE)
+    re_Finished         = re.compile(r"%(PLYR)s:? finished \d+ out of \d+ players" %  substitutions, re.MULTILINE)
     re_Dealer           = re.compile(r"Dealer:") #Some Cake hands just omit the game line so we can just discard them as partial
     re_CoinFlip         = re.compile(r"Coin\sFlip\sT\d+", re.MULTILINE)
     re_ReturnBet        = re.compile(r"returns\suncalled\sbet", re.MULTILINE)
@@ -376,7 +376,7 @@ class Cake(HandHistoryConverter):
                     cards = cards.split(' ') # needs to be a list, not a set--stud needs the order
                 elif m.group('SHOWED') == "mucks":
                     mucked = True
-                    cards = cards.split(',') # needs to be a list, not a set--stud needs the order
+                    cards = [c.strip() for c in cards.split(',')] # needs to be a list, not a set--stud needs the order
 
                 #print "DEBUG: hand.addShownCards(%s, %s, %s, %s)" %(cards, m.group('PNAME'), shown, mucked)
                 try:
