325d9356d20ce3158ff8a5d053357823249af532
gimick <bbtgaf@googlemail.com>
2013-05-11 21:01:41 +0100

    py2exe: bring in *Summary modules

--- packaging/windows/py2exe_setup.py
+++ packaging/windows/py2exe_setup.py
@@ -120,8 +120,10 @@ packagedir = rootdir+'packaging/windows/'
 gfxdir = rootdir+'gfx/'
 sys.path.append(pydir)  # allows fpdb modules to be found in the setup() below
 tofpdb_file_list = fnmatch.filter(os.listdir(pydir), '*ToFpdb.py')
+summary_file_list = fnmatch.filter(os.listdir(pydir), '*Summary.py')
 #convert to module list by removing extensions in this list comprehension
 tofpdb_module_list = [os.path.splitext(filename)[0] for filename in tofpdb_file_list]
+summary_module_list = [os.path.splitext(filename)[0] for filename in summary_file_list]
 fpdb_aux_module_list = ['Hello','Aux_Hud','Aux_Classic_Hud','Mucked']
 
 print "\n" + r"Output will be created in "+distdir
@@ -159,7 +161,7 @@ setup(
                       'packages'    : ['encodings', 'matplotlib', 'BeautifulSoup'],
                                                             
                       'includes'    : ['gio', 'cairo', 'pango', 'pangocairo'
-				      ,'atk', 'gobject'] + tofpdb_module_list + fpdb_aux_module_list,
+				      ,'atk', 'gobject'] + tofpdb_module_list + summary_module_list + fpdb_aux_module_list,
                                       
                       'excludes'    : ['_tkagg', '_agg2', 'cocoaagg', 'fltkagg'],
                       
