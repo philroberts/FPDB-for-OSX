384dcc991a0cf2c4f4e2ef928dca8a43bb5c2625
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2014-07-01 17:04:26 +0000

    HM1 exported stars hands not counting blinds as a prior bets so addRaiseBy is inaccurate - swiching to addRaiseTo

--- pyfpdb/PokerStarsToFpdb.py
+++ pyfpdb/PokerStarsToFpdb.py
@@ -527,7 +527,7 @@ class PokerStars(HandHistoryConverter):
             elif action.group('ATYPE') == ' calls':
                 hand.addCall( street, action.group('PNAME'), action.group('BET') )
             elif action.group('ATYPE') == ' raises':
-                hand.addRaiseBy( street, action.group('PNAME'), action.group('BET') )
+                hand.addRaiseTo( street, action.group('PNAME'), action.group('BETTO') )
             elif action.group('ATYPE') == ' bets':
                 hand.addBet( street, action.group('PNAME'), action.group('BET') )
             elif action.group('ATYPE') == ' discards':
