f401fd8a6a2691032afb7c92417c983b3c818f49
Phil Roberts <philip.roberts@gmail.com>
2014-10-19 20:40:41 +1030

    Port GuiBulkImport and Importer to Qt.

--- pyfpdb/GuiBulkImport.py
+++ pyfpdb/GuiBulkImport.py
@@ -25,11 +25,7 @@ from time import time
 from optparse import OptionParser
 import traceback
 
-#    pyGTK modules
-import pygtk
-pygtk.require('2.0')
-import gtk
-import gobject
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QFileDialog
 
 #    fpdb/FreePokerTools modules
 import Options
@@ -43,17 +39,11 @@ if __name__ == "__main__":
 # logging has been set up in fpdb.py or HUD_main.py, use their settings:
 log = logging.getLogger("importer")
 
-class GuiBulkImport():
-
+class GuiBulkImport(QWidget):
     # CONFIGURATION  -  update these as preferred:
     allowThreads = False  # set to True to try out the threads field
 
-    def dopulse(self):
-        self.progressbar.pulse()
-        return True
-
-    def load_clicked(self, widget, data=None):
-
+    def load_clicked(self):
         stored = None
         dups = None
         partial = None
@@ -63,32 +53,20 @@ class GuiBulkImport():
         # (see comment above about what to do if pipe already open)
         if self.settings['global_lock'].acquire(wait=False, source="GuiBulkImport"):   # returns false immediately if lock not acquired
             #try:
-                self.progressbar.set_text(_("Importing"))
-                self.progressbar.pulse()
-                while gtk.events_pending(): # see http://faq.pygtk.org/index.py?req=index for more hints (3.7)
-                    gtk.main_iteration(False)
-                self.timer = gobject.timeout_add(100, self.dopulse)
-
                 #    get the dir to import from the chooser
-                selected = self.chooser.get_filenames()
+                selected = self.importDir.text()
 
                 #    get the import settings from the gui and save in the importer
                 
                 self.importer.setHandsInDB(self.n_hands_in_db)
                 self.importer.setMode('bulk')
 
-                sitename = 'auto'
-
- 
-                for selection in selected:
-                    self.importer.addBulkImportImportFileOrDir(selection, site = sitename)
+                self.importer.addBulkImportImportFileOrDir(selected, site = 'auto')
                 self.importer.setCallHud(False)
                 
                 starttime = time()
 
                 (stored, dups, partial, errs, ttime) = self.importer.runImport()
-
-                gobject.source_remove(self.timer)
 
                 ttime = time() - starttime
                 if ttime == 0:
@@ -101,63 +79,34 @@ class GuiBulkImport():
 
                 self.importer.clearFileList()
                 
-                self.progressbar.set_text(_("Import Complete"))
-                self.progressbar.set_fraction(0)
-           
                 self.settings['global_lock'].release()
         else:
             print _("bulk import aborted - global lock not available")
 
     def get_vbox(self):
         """returns the vbox of this thread"""
-        return self.vbox
+        return self.layout()
 
     def __init__(self, settings, config, sql = None, parent = None):
+        QWidget.__init__(self, parent)
         self.settings = settings
         self.config = config
-        self.parent = parent
 
-        self.importer = Importer.Importer(self, self.settings, config, sql, parent)
+        self.importer = Importer.Importer(self, self.settings, config, sql, self)
 
-        self.vbox = gtk.VBox(False, 0)
-        self.vbox.show()
+        self.setLayout(QVBoxLayout())
 
-        self.chooser = gtk.FileChooserWidget()
-        self.chooser.set_filename(self.settings['bulkImport-defaultPath'])
-        self.chooser.set_select_multiple(True)
-        self.chooser.set_show_hidden(True)
-        self.vbox.add(self.chooser)
-        self.chooser.show()
+        self.importDir = QLineEdit(self.settings['bulkImport-defaultPath'])
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.importDir)
+        self.chooseButton = QPushButton('Browse...')
+        self.chooseButton.clicked.connect(self.browseClicked)
+        hbox.addWidget(self.chooseButton)
+        self.layout().addLayout(hbox)
 
-#    Table widget to hold the progress bar and load button
-        #self.table = gtk.Table(rows=5, columns=5, homogeneous=False)
-        self.table = gtk.Table(rows=2, columns=4, homogeneous=False)
-        self.vbox.add(self.table)
-        self.table.show()
-
-#    label - spacer (fills in column one in table)
-        self.lab_spacer = gtk.Label()
-#         self.table.attach(self.lab_spacer, 3, 5, 3, 4, xpadding=0, ypadding=0,
-#                           yoptions=gtk.SHRINK)
-        self.table.attach(self.lab_spacer,1,2,1,2, xpadding=0, ypadding=0, yoptions=gtk.SHRINK)
-        self.lab_spacer.show()
-
-#    button - Import
-        self.load_button = gtk.Button(_('_Bulk Import'))  # todo: rename variables to import too
-        self.load_button.connect('clicked', self.load_clicked,
-                                 _('Import clicked'))
-#         self.table.attach(self.load_button, 2, 3, 4, 5, xpadding=0, ypadding=0,
-#                           yoptions=gtk.SHRINK)
-        self.table.attach(self.load_button, 2, 3, 1, 2, xpadding=0, ypadding=0, yoptions=gtk.SHRINK)
-        self.load_button.show()
-
-#    label - info
-
-        self.progressbar = gtk.ProgressBar()
-        self.table.attach(self.progressbar, 3, 4, 1, 2, xpadding=0, ypadding=0, yoptions=gtk.SHRINK)
-        self.progressbar.set_text(_("Waiting..."))
-        self.progressbar.set_fraction(0)
-        self.progressbar.show()
+        self.load_button = QPushButton(_('_Bulk Import'))
+        self.load_button.clicked.connect(self.load_clicked)
+        self.layout().addWidget(self.load_button)
 
 #    see how many hands are in the db and adjust accordingly
         tcursor = self.importer.database.cursor
@@ -167,31 +116,14 @@ class GuiBulkImport():
         self.importer.database.rollback()
         self.n_hands_in_db = row[0]
 
-def main(argv=None):
-    """main can also be called in the python interpreter, by supplying the command line as the argument."""
-    if argv is None:
-        argv = sys.argv[1:]
+    def browseClicked(self):
+        newdir = QFileDialog.getExistingDirectory(self, caption=_("Please choose the path that you want to Auto Import"),
+                                            directory=self.importDir.text())
+        if newdir:
+            self.importDir.setText(newdir)
 
-    def destroy(*args):  # call back for terminating the main eventloop
-        gtk.main_quit()
-
-    Configuration.set_logfile("fpdb-log.txt")
-    (options, argv) = Options.fpdb_options()
-
-    if options.sitename:
-        options.sitename = Options.site_alias(options.sitename)
-
-    if options.usage == True:
-        #Print usage examples and exit
-        print _("USAGE:")
-        sys.exit(0)
-
-    Configuration.set_logfile("GuiBulkImport-log.txt")
-    if options.config:
-        config = Configuration.Config(options.config)
-    else:
-        config = Configuration.Config()
-
+if __name__ == '__main__':
+    config = Configuration.Config()
     settings = {}
     if os.name == 'nt': settings['os'] = 'windows'
     else:               settings['os'] = 'linuxmac'
@@ -199,21 +131,14 @@ def main(argv=None):
     settings.update(config.get_db_parameters())
     settings.update(config.get_import_parameters())
     settings.update(config.get_default_paths())
+    import interlocks, string
+    settings['global_lock'] = interlocks.InterProcessLock(name="fpdb_global_lock")
+    settings['cl_options'] = string.join(sys.argv[1:])
 
-    #Do something useful
-    importer = Importer.Importer(False,settings, config, None)
-    importer.addBulkImportImportFileOrDir(os.path.expanduser(options.filename))
-    importer.setCallHud(False)
-    if options.archive:
-        importer.setStarsArchive(True)
-        importer.setFTPArchive(True)
-    if options.testData:
-        importer.setPrintTestData(True)
-    (stored, dups, partial, errs, ttime) = importer.runImport()
-    importer.clearFileList()
-    print(_('Bulk import done: Stored: %d, Duplicates: %d, Partial: %d, Errors: %d, Time: %s seconds, Stored/second: %.0f')\
-                     % (stored, dups, partial, errs, ttime, (stored+0.0) / ttime))
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+    from PyQt5.QtWidgets import QApplication, QMainWindow
+    app = QApplication([])
+    main_window = QMainWindow()
+    main_window.setCentralWidget(GuiBulkImport(settings, config))
+    main_window.show()
+    main_window.resize(600, 100)
+    app.exec_()
