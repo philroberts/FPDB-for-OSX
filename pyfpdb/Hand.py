55896a841e100ba5e524634dd93361cf260c3fe4
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2015-02-10 13:03:10 +0000

    Added siteHandNo logging back in

--- pyfpdb/Hand.py
+++ pyfpdb/Hand.py
@@ -303,6 +303,7 @@ class Hand(object):
         
     def getHandId(self, db, id):
         if db.isDuplicate(self.siteId, self.hands['siteHandNo'], self.hands['heroSeat'], self.publicDB):
+            #log.debug(_("Hand.insert(): hid #: %s is a duplicate") % self.hands['siteHandNo'])
             self.is_duplicate = True  # i.e. don't update hudcache
             next = id
             raise FpdbHandDuplicate(self.hands['siteHandNo'])
