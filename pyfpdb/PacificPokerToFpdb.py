philroberts/FPDB-for-OSX

commit 5ef008b36c9bdbd817ab89d2a5e5472d86343a21

PacificPokerToFpdb.markStreets is a pretty good sign that the hand was cancelled so swiching this to a FpdbHandPartial error

pyfpdb/PacificPokerToFpdb.py
@@ -310,8 +310,8 @@ class PacificPoker(HandHistoryConverter):
                        r"(\*\* Dealing river \*\* (?P<RIVER>\[ \S\S \].+?(?=\*\* Summary \*\*)|.+))?"
                        , hand.handText,re.DOTALL)
         if m is None:
-            log.error(_("PacificPokerToFpdb.markStreets: Unable to recognise streets"))
-            raise FpdbParseError
+            #log.error(_("PacificPokerToFpdb.markStreets: Unable to recognise streets"))
+            raise FpdbHandPartial
         else:
             #print "DEBUG: Matched markStreets"
             mg = m.groupdict()

commit 9e236a1475f9c938febd45fc96e606fca1173a0f

888Poker observed hands don't have a '** Dealing down cards **' line. markStreets works fine without that pattern. Regression from 5ef008b now imports

pyfpdb/PacificPokerToFpdb.py
@@ -301,10 +301,10 @@ class PacificPoker(HandHistoryConverter):
             hand.addPlayer(int(a.group('SEAT')), a.group('PNAME'), self.clearMoneyString(a.group('CASH')))
 
     def markStreets(self, hand):
-        # PREFLOP = ** Dealing down cards **
+        # PREFLOP = ** Dealing down cards ** (observed hands don't have this line)
         # This re fails if,  say, river is missing; then we don't get the ** that starts the river.
         if hand.gametype['base'] in ("hold"):
-            m =  re.search(r"\*\* Dealing down cards \*\*(?P<PREFLOP>.+(?=\*\* Dealing flop \*\*)|.+)"
+            m =  re.search(r"(?P<PREFLOP>.+(?=\*\* Dealing flop \*\*)|.+)"
                        r"(\*\* Dealing flop \*\* (?P<FLOP>\[ \S\S, \S\S, \S\S \].+(?=\*\* Dealing turn \*\*)|.+))?"
                        r"(\*\* Dealing turn \*\* (?P<TURN>\[ \S\S \].+(?=\*\* Dealing river \*\*)|.+))?"
                        r"(\*\* Dealing river \*\* (?P<RIVER>\[ \S\S \].+?(?=\*\* Summary \*\*)|.+))?"